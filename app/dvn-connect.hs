233ae76b783d426d32e32c9a8dba75665d3c495a
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-02-11 19:32:29 +0900

    trim the list of dependencies

--- app/dvn-connect.hs
+++ app/dvn-connect.hs
@@ -3,5 +3,6 @@ import qualified Data.ByteString.Char8 as B
 import qualified Network.WebSockets as WS
 import qualified Wuss as WS
 
+main :: IO ()
 main = WS.runSecureClient "gateway.discord.gg" 443 "/?v=6&encoding=json"
   $ \wsConn -> forever $ WS.receiveData wsConn >>= B.putStrLn
